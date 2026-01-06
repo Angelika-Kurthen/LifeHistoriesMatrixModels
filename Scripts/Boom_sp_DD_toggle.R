b14c5c775249f2de9886ea75dee1acf1d81ae93e
Angelika Kurthen <kurthena@oregonstate.edu>
2026-01-06 11:37:35 -0800

    adding in some comments

--- Scripts/Boom_sp_DD_toggle.R
+++ Scripts/Boom_sp_DD_toggle.R
@@ -1,18 +1,23 @@
 #############################
-# B sp degree day toggle
+# Boom Life History degree day toggle
 ############################
 library(lubridate)
-#Code for HPC
-#library(lubridate, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
 
+# source datasets
+# Boom_1sp_Model.R: Boom Life History population model
+# 1spFunctions.R: bespoke functions
+# NegExpSurv.R: negative exponential survival function for disturbance
+source("Scripts/Boom_1sp_Model.R")
+source("Scripts/1spFunctions.R")
+source("Scripts/NegExpSurv.R")
 
-
-source("LifeHistoriesMatrixModels/Scripts/B_1sp_Model.R")
-source("LifeHistoriesMatrixModels/Scripts/1spFunctions.R")
-source("LifeHistoriesMatrixModels/Scripts/NegExpSurv.R")
-
+# Simulation time in days (~100 years)
 Time <- c(1:36500)
+
+# Day-of-year index repeated for 100 years
 Date <- rep(1:365, times = 100)
+
+# Actual calendar dates spanning 2022–2121
 Day <- seq(as.Date("2022-01-01"), as.Date("2121-12-31"), by="days")
 # find and remove leap days
 find_leap = function(x){
@@ -20,20 +25,27 @@ find_leap = function(x){
 }
 Day <- Day[which(find_leap(Day) == F)]
 
-
+# Generate seasonal temperature time series
 Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 13.956243
 
+# Combine time, date, and temperature into a data frame
 temp <- as.data.frame(cbind(Time, Day, Temperature))
 temp$Day <- as.Date(temp$Day, origin= "1970-01-01")
 colnames(temp) <- c("Time", "Date", "Temperature")
+
+# Convert to timestep-based temperature object
 temp <- TimestepTemperature(temp)
+# Keep only timestep and temperature columns
 temp <- temp[c(1,3)]
 
 Year <- year(temp$dts)
 uYear <- unique(Year)
 Month <- month(temp$dts)
+
+# no disturbance, no hydropeaking
 discharge <- rep(0.1, time = length(temp$dts))
 
+# Degree-day requirement sensitivity range ±10% around baseline degree-day requirement (500)
 dd_seq <- seq(500*0.9, 500*1.1, length.out = 21)
 
 
