ffb5bd86ef8fbc664937f6dd9ce38768d83ecef4
Angelika Kurthen <kurthena@oregonstate.edu>
2026-01-06 11:54:44 -0800

    added some comments

--- Scripts/Moderate_sp_Fecundity_Toggle.R
+++ Scripts/Moderate_sp_Fecundity_Toggle.R
@@ -1,42 +1,56 @@
 #############################
-# A sp fecundity toggle
+# Moderate Life History fecundity toggle
 ############################
 library(lubridate)
-#Code for HPC
-#library(lubridate, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
 
+# Moderate_1sp_Model.R: Moderate Life History population model
+# 1spFunctions.R: bespoke functions
+# NegExpSurv.R: negative exponential survival function for disturbance
+source("Scripts/Moderate_1sp_Model.R")
+source("Scripts/1spFunctions.R")
+source("Scripts/NegExpSurv.R")
 
-source("LifeHistoriesMatrixModels/Scripts/A_1sp_Model.R")
-source("LifeHistoriesMatrixModels/Scripts/1spFunctions.R")
-source("LifeHistoriesMatrixModels/Scripts/NegExpSurv.R")
-
+# Simulation time in days (~100 years)
 Time <- c(1:36500)
+
+# Day-of-year index repeated for 100 years
 Date <- rep(1:365, times = 100)
+
+# Actual calendar dates spanning 2022–2121
 Day <- seq(as.Date("2022-01-01"), as.Date("2121-12-31"), by="days")
+
 # find and remove leap days
 find_leap = function(x){
   day(x) == 29 & month(x) == 2 
 }
 Day <- Day[which(find_leap(Day) == F)]
 
-
+# Generate seasonal temperature time series
 Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 13.956243
 
+# Combine time, date, and temperature into a data frame
 temp <- as.data.frame(cbind(Time, Day, Temperature))
 temp$Day <- as.Date(temp$Day, origin= "1970-01-01")
 colnames(temp) <- c("Time", "Date", "Temperature")
+
+# Convert to timestep-based temperature object
 temp <- TimestepTemperature(temp)
+
+# Keep only timestep and temperature columns
 temp <- temp[c(1,3)]
 
 Year <- year(temp$dts)
 uYear <- unique(Year)
 Month <- month(temp$dts)
+
+# no disturbance, no hydropeaking
 discharge <- rep(0.1, time = length(temp$dts))
 
+# Degree-day requirement sensitivity range ±10% around baseline fecundity 
 fec_seq <- seq(500*0.9, 500*1.1, length.out = 21)
 
 
-# Run A sp (itermediate response to different scenarios) with no HFE, no hydropeaking (so no disturbance), temperature of North American temperate stream between 3 and 18 C, and fecundities from between 20 and 2000 eggs per brood. 
+# Run single-species model across frequency values
 fec_means <- vector()
 for (fec in 1:length(fec_seq)){
   print(fec)
@@ -45,13 +59,10 @@ for (fec in 1:length(fec_seq)){
   fec_means[fec] <- mean(means.list.C$mean.abund)
 }
 
+# Assemble results 
 a_fec_df <- as.data.frame(cbind(fec_seq, fec_means, rep("A", times = length(fec_means))))
 a_fec_df$fec_seq <- as.numeric(a_fec_df$fec_seq)
 a_fec_df$fec_means <- as.numeric(a_fec_df$fec_means)
+
+# Fit linear model: relative abundance vs fecundity
 afec_lm <- lm((fec_means/10000) ~ fec_seq, data = a_fec_df)
-summary(afec_lm)
-# afec <- ggplot(data = fec_df, mapping = aes(x = fec_seq, y = fec_means/10000))+
-#   geom_point(size  = 1, col = "#66CCEE")+
-#   xlab("Egg Mass Size")+
-#   ylab("A sp Abundance Relative to K")+
-#   theme_bw()
