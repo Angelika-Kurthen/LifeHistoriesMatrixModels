36f2f1ebb23621861412cda86612a34588cbc1c8
Angelika Kurthen <kurthena@oregonstate.edu>
2026-01-06 11:33:18 -0800

    adding more detailed comments

--- Scripts/Fast_sp_DD_toggle.R
+++ Scripts/Fast_sp_DD_toggle.R
@@ -1,19 +1,23 @@
 #############################
-# A sp degree day toggle
+# Fast Life History degree day toggle
 ############################
-
-#Code for HPC
-#library(lubridate, lib.loc = "/home/ib/kurthena/R_libs/4.2.1")
 
 library(lubridate)
 
+# Fast_1sp_Model.R: Fast Life History population model
+# 1spFunctions.R: bespoke functions
+# NegExpSurv.R: negative exponential survival function for disturbance
+source("Scripts/Fast_1sp_Model.R")
+source("Scripts/1spFunctions.R")
+source("Scripts/NegExpSurv.R")
 
-source("LifeHistoriesMatrixModels/Scripts/A_1sp_Model.R")
-source("LifeHistoriesMatrixModels/Scripts/1spFunctions.R")
-source("LifeHistoriesMatrixModels/Scripts/NegExpSurv.R")
-
+# Simulation time in days (~100 years)
 Time <- c(1:36500)
+
+# Day-of-year index repeated for 100 years
 Date <- rep(1:365, times = 100)
+
+# Actual calendar dates spanning 2022–2121
 Day <- seq(as.Date("2022-01-01"), as.Date("2121-12-31"), by="days")
 # find and remove leap days
 find_leap = function(x){
@@ -21,39 +25,43 @@ find_leap = function(x){
 }
 Day <- Day[which(find_leap(Day) == F)]
 
-
+# Generate seasonal temperature time series
 Temperature <-  -7.374528  * (cos(((2*pi)/365)*Date))  +  (-1.649263* sin(2*pi/(365)*Date)) + 13.956243
 
+# Combine time, date, and temperature into a data frame
 temp <- as.data.frame(cbind(Time, Day, Temperature))
 temp$Day <- as.Date(temp$Day, origin= "1970-01-01")
 colnames(temp) <- c("Time", "Date", "Temperature")
+
+# Convert to timestep-based temperature object
 temp <- TimestepTemperature(temp)
+
+# Keep only timestep and temperature columns
 temp <- temp[c(1,3)]
 
 Year <- year(temp$dts)
 uYear <- unique(Year)
 Month <- month(temp$dts)
+
+# no disturbance, no hydropeaking
 discharge <- rep(0.1, time = length(temp$dts))
 
+# Degree-day requirement sensitivity range ±10% around baseline degree-day requirement (1200)
 dd_seq <- seq(1200*0.9, 1200*1.1, length.out = 21)
 
-
-# Run A sp (itermediate response to different scenarios) with no HFE, no hydropeaking (so no disturbance), temperature of North American temperate stream between 3 and 18 C, and fecundities from between 20 and 2000 eggs per brood. 
+# Run single-species model across DD values
 dd_means <- vector()
 for (dd in 1:length(dd_seq)){
-  print(dd)
   out <- Amodel(discharge, temp, baselineK = 10000, disturbanceK = 40000, Qmin = 0.25, extinct = 50, iteration = 2, peaklist = 0, peakeach = length(temp$Temperature), dds = dd_seq[dd])
+  # Calculate mean abundance after burn-in
   means.list.A <- mean.data.frame(out, burnin = 250, iteration = 2) 
   dd_means[dd] <- mean(means.list.A$mean.abund)
 }
 
+# Assemble results 
 add_df <- as.data.frame(cbind(dd_seq, dd_means, rep("A", length(dd_means))))
 add_df$dd_seq <- as.numeric(add_df$dd_seq)
 add_df$dd_means <- as.numeric(add_df$dd_seq)
 
+# Fit linear model: relative abundance vs DD requirement
 add_lm <- lm((dd_means/10000) ~ dd_seq, data= add_df)
-# add <- ggplot(data = dd_df, mapping = aes(x = dd_seq, y = dd_means/10000))+
-#   geom_point(size = 1, col= "#66CCEE")+
-#   xlab("Degree Day Requirement")+
-#   ylab("A sp Abundance Relative to K")+
-#   theme_bw()
